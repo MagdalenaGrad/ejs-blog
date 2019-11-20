6e71d5929e0182a65c4a4ac1f80d44e8bbf69c27
Magdalena Grad <mgdln.grad@gmail.com>
2019-11-20 17:00:01 +0100

    add compose page 7

--- app.js
+++ app.js
@@ -31,14 +31,15 @@ app.get('/about', function (req, res) {
   });
 });
 
-
 app.get('/contact', function (req, res) {
   res.render('contact', {
     contactP: contactContent
   });
 });
 
-
+app.get('/compose', function (req, res) {
+  res.render('compose');
+})
 
 
 
