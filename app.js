58e6174e4669bfc048a09de64e859ed50d19bcd2
Magdalena Grad <mgdln.grad@gmail.com>
2019-11-20 15:05:17 +0100

    add home route 1

--- app.js
+++ app.js
@@ -12,10 +12,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
+app.get('/', function (req, res) {
+  // res.render(__dirname + '/views/home.ejs');
+  res.render('home');
+})
 
 
 
@@ -27,7 +33,6 @@ app.use(express.static("public"));
 
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
